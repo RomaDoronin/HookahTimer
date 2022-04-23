c711bb121cf17355d8bacccae784710090544a5e
Roman Doronin <rdoronin@sberhealth.ru>
2022-04-23 11:15:41 +0300

    Добавлен проект

---
+++ HookahTimer/HookahTimer/Application/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  HookahTimer
+//
+//  Created by Roman Doronin on 23.04.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+}
+
---
+++ HookahTimer/HookahTimer/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HookahTimer
+//
+//  Created by Roman Doronin on 23.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+    }
+
+}
